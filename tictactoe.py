8739d23c44659236492a0582ccdbb682c8be40f3
rlorimier <rere.rnt@gmail.com>
2022-03-28 09:55:49 +0100

    add color and master function to check winner

--- tictactoe.py
+++ tictactoe.py
@@ -9,6 +9,8 @@ player = "X" #first player is always X
 winner = None
 game_running = True
 
+print("\033[95m-- TIC TAC TOE --\033[m\n")
+
 #creating the board game
 def print_board(board):
     print("-" *10)
@@ -34,40 +36,52 @@ def check_horiz(board):
     global winner
     if board[0] == board[1] == board[2] and board[0] != "*":
         winner = board[0]
+        return True
     elif board[3] == board[4] == board[5] and board[3] != "*":
         winner = board[3]
+        return True
     elif board[6] == board[7] == board[8] and board[6] != "*":
         winner = board[6]
+        return True
 
 #checking for winners vertically
 def check_vert(board):
     global winner
     if board[0] == board[3] == board[6] and board[0] != "*":
         winner = board[0]
+        return True
     elif board[1] == board[4] == board[7] and board[1] != "*":
         winner = board[1]
+        return True
     elif board[2] == board[5] == board[8] and board[2] != "*":
         winner = board[2]
+        return True
 
 #checking for winners diagonally
 def check_diag(board):
     global winner
     if board[0] == board[4] == board[8] and board[0] != "*":
         winner = board[0]
+        return True
     elif board[2] == board[4] == board[6] and board[2] != "*":
         winner = board[2]
+        return True
     
 
+#main function to check the winner
 def check_winner():
     global game_running
     if check_horiz(board) or check_vert(board) or check_diag(board):
+        print_board(board)
         print(f"The winner is {winner}")
         game_running = False
+
 
 #checking for tie
 def chack_tie(board):
     global game_running
     if "*" not in board:
+        print_board(board)
         print("It is a tie!")
         game_running = False
 
@@ -81,16 +95,14 @@ def switch_player():
         player = "X"
 
 #all functions
-def main():
-    while game_running == True:
-        print_board(board)
-        player_input(board)
-        check_winner()
-        chack_tie(board)
-        switch_player()
+while game_running:
+    print_board(board)
+    player_input(board)
+    check_winner()
+    chack_tie(board)
+    switch_player()
         
         
 
 
-print("-- TIC TAC TOE --\n")
-main()
+print("\033[93mGame Over\033[m")
